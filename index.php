da1a21ba25a264b032a3ba648ac154090f6868d2
grzegorzpryjma99 <62578864+grzegorzpryjma99@users.noreply.github.com>
2020-12-07 17:11:33 +0100

    Create first php routing

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "Elo byku ";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', "DefaultController");
+Routing::get('home', "DefaultController");
+Routing::run($path);
