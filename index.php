c27b82be196dd81851b449c6b63c490bb59cf8bb
grzegorzpryjma99 <62578864+grzegorzpryjma99@users.noreply.github.com>
2020-12-12 18:19:55 +0100

    authorization

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
 require 'Routing.php';
-
+//phpinfo();
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
-Routing::get('index', "DefaultController");
+Routing::get('', "DefaultController");
 Routing::get('home', "DefaultController");
+Routing::post('login', "SecurityController");
+
+
 Routing::run($path);
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -5,12 +5,12 @@ require_once 'AppController.php';
 class DefaultController extends AppController{
 
     public function index(){
-        //TODO display login.html
+
         $this->render('login');
     }
 
     public function home(){
-        //TODO display home.html
+        //TODO display home.php
         $this->render('home');
 
     }
