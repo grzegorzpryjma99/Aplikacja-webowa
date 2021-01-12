21a2198ddd0ca168b6fc7e0f2ef95ecf77f66475
grzegorzpryjma99 <62578864+grzegorzpryjma99@users.noreply.github.com>
2021-01-12 17:34:46 +0100

    Create database

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+const USERNAME = 'ikocmumnpiodav';
+const PASSWORD = '493ea2d1f2d6239f26ecc16360688be3d083d3f8f1ce24ae6f1d5b9679abd398';
+const HOST = 'ec2-54-155-99-116.eu-west-1.compute.amazonaws.com';
+const DATABASE = 'd1j2l1dm55gr5h';
--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,15 +2,15 @@
 //logowanie i to do rejestracja
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
-
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController {
 
 
     public function login()
     {
-        //fikcyjny uzytkownik do czasu zrobienia bazy
-        $user = new User('jsnow@pk.edu.pl', 'admin', 'Johnny', 'Snow');
+        $userRepository = new UserRepository();
+
 
         //sprawdzam czy to POST z formularza a mam to z widoku (method="POST")
         if (!$this->isPost()) {
@@ -19,6 +19,12 @@ class SecurityController extends AppController {
         //te nazwy zmiennych biorą sie z formularza login.php ZOBACZ
         $email = $_POST['email'];
         $password = $_POST['password'];
+
+        $user = $userRepository->getUser($email);
+
+        if(!$user){
+            return $this->render('login', ['messages' => ['User not found!']]);
+        }
 
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['User with this email not exist!']]);
